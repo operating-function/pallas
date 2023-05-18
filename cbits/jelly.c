fd5ba5a8f129b2e8d671aeab45c2fc6015ec0cac
Sol <sol@plunder.tech>
2023-05-18 10:15:07 -0400

    jelly: build on aarch64-darwin
    This is pretty jank, but makes the situation workable for now.
    
    -   This switches on `__clang__` in order to check if we are using glibc,
        which is clearly wrong but works for now.  `_GNU_SOURCE` and
        `__GNUC__` didn't work.
    
    -   `ghcid` doesn't build on darwin for some reason, and you can't have
        impure conditionals in `flakes.nix`.

--- cbits/jelly.c
+++ cbits/jelly.c
@@ -4,11 +4,27 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <errno.h>
 
 #include "jelly.h"
 #include "xxh3.h"
 #include "libbase58.h"
 #include "blake3.h"
+
+
+// Shims ///////////////////////////////////////////////////////////////////////
+
+#ifdef __clang__
+#define MUL_NO_OVERFLOW ((size_t)1 << (sizeof(size_t) * 4))
+void *reallocarray(void *optr, size_t nmemb, size_t size) {
+    if ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) &&
+         nmemb > 0 && SIZE_MAX / nmemb < size) {
+        errno = ENOMEM;
+        return NULL;
+    }
+    return realloc(optr, size * nmemb);
+}
+#endif
 
 
 // Forward Declarations ////////////////////////////////////////////////////////
