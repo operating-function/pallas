a887a26a504dcabb034356384d4136bdabcdd2dd
Sol <sol@plunder.tech>
2023-06-13 02:57:30 -0400

    sire: misc minor cleanup

--- lib/Fan/JetHash.hs
+++ lib/Fan/JetHash.hs
@@ -155,7 +155,7 @@ jetHashes
     , e "tabElemIdx"          "E6FfxpnVgZ6peeviKPS9KAsmT3JYxutgicWdj6PrZazr"
     , e "tabSplitAt"          "CGzUhtCE3ftpYXngQMEPJLE8d2Ttxhzen6FN8kYndSaN"
     , e "tabSplitLT"          "3piVjrexs2LYLr5593fUvNoLq5giWSzjHRPdg4SntjVw"
-    , e "tabAlter"            "h3acwX8hLfAtBSJErLTnsrP35w4p1ZxqaqeAoRF5oc9"
+    , e "tabAlter"            "5Lb18qCr2ZKdc6bds7VhFkUtmjaAnhi8TjV3XTM6j9DR"
     , e "tabMapWithKey"       "7rB3mSNqPfnrjk3pD1VyAqbuSZyCS1AM7TbERYDTiGSi"
     , e "tabUnionWith"        "83F6j6LwXCjPy1C6n5CEjuQ5p8FMK4poZhgnBDrFj3yz"
     , e "tabMinKey"           "6dq3qQAA8FcehnUMKycJeke7dQMqECcrwZJADxmCpKce"
